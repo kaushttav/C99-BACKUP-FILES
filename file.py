30347238e4d25f7422924c545dd2a78c14f6a4b3
kaushttav <59498306+kaushttav@users.noreply.github.com>
2021-10-14 17:46:42 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+path = input("Enter the name of the directory to be sorted : ")
+num = os.listdir(path)
+
+for file in num :
+    name, ext = os.path.splitext(file)
+    ext = ext[1:]
+    if ext == '':
+        continue
+    if os.path.exists(path + '/' + ext) : 
+        shutil.move(path+'/'+file, path+'/'+ext+'/'+file)
+    else :
+        os.makedirs(path + '/' + ext)
+        shutil.move(path+'/'+file, path+'/'+ext+'/'+file)
+
+    
